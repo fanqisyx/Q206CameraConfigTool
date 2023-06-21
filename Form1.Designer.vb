f215dbd258d2893dd6597e122c8fb30ad6d828b7
fanqi <fanqi@DESKTOP-EO60T56>
2023-06-21 09:09:46 +0800

    添加项目文件。

---
+++ Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(1058, 625)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.Button2)
+        Me.TabPage1.Controls.Add(Me.TextBox1)
+        Me.TabPage1.Controls.Add(Me.Button1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(1050, 599)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "相机信息读写"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(1050, 599)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "本地信息读写"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Size = New System.Drawing.Size(1050, 599)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "汇总信息读写"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(8, 6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(91, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "相机信息读取"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.TextBox1.Location = New System.Drawing.Point(3, 35)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(1044, 561)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(105, 6)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(91, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "写入csv"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1058, 625)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,74 @@
+﻿Imports System.IO
+Public Class Form1
+    'textbox：
+    Delegate Sub AddShowTextBoxDelegate(textbox As System.Windows.Forms.TextBox, str As String) '//托管程序
+    ''' <summary>
+    ''' 修改TextBox中内容
+    ''' </summary>
+    ''' <param name="textbox">TextBox控件的名称</param>
+    ''' <param name="str">需要修改的值</param>
+    ''' <remarks></remarks>
+    Private Sub DG_ChangeTextBoxValue(textbox As System.Windows.Forms.TextBox, str As String) '//托管程序
+        If (textbox.InvokeRequired) Then
+            Dim d As New AddShowTextBoxDelegate(AddressOf DG_ChangeTextBoxValue)
+            textbox.Invoke(d, textbox, str)
+        Else
+            textbox.Text = str
+        End If
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        GetCamerainfo()
+    End Sub
+
+    Sub GetCamerainfo()
+        Try
+            Dim DeviceList As System.Collections.Generic.List(Of ThridLibray.IDeviceInfo) =
+                (ThridLibray.Enumerator.EnumerateDevices())
+            MessageBox.Show("发现相机数量：" + DeviceList.Count.ToString())
+            If DeviceList.Count > 0 Then
+                Dim info As String = ""
+                info = "Index,Key,ManufactureInfo,Model,Name,SerialNumber,Vendor,Version" + Environment.NewLine
+                For Each device In DeviceList
+                    info += device.Index.ToString() + ","
+                    info += device.Key.ToString() + ","
+                    info += device.ManufactureInfo.ToString() + ","
+                    info += device.Model.ToString() + ","
+                    info += device.Name.ToString() + ","
+                    info += device.SerialNumber.ToString() + ","
+                    info += device.Vendor.ToString() + ","
+                    info += device.Version.ToString()
+                    info += Environment.NewLine
+                Next
+                DG_ChangeTextBoxValue(TextBox1, info)
+            End If
+        Catch ex As Exception
+            DG_ChangeTextBoxValue(TextBox1, "在加载相机过程中失败：" + ex.ToString())
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim filePath As String = "LocalCameraInfo.csv"
+        Dim content As String = TextBox1.Text
+
+        ' 检查文件是否存在
+        If File.Exists(filePath) Then
+            Dim result As DialogResult = MessageBox.Show("文件已存在。是否覆盖？", "提示", MessageBoxButtons.YesNo)
+
+            If result = DialogResult.Yes Then
+                WriteToFile(filePath, content, True) ' 覆盖现有文件
+            End If
+        Else
+            WriteToFile(filePath, content, False) ' 创建新文件
+        End If
+    End Sub
+
+
+
+    Sub WriteToFile(ByVal filePath As String, ByVal content As String, ByVal overwrite As Boolean)
+        Using writer As StreamWriter = New StreamWriter(filePath, overwrite)
+            writer.Write(content)
+        End Using
+
+        Console.WriteLine("内容已写入文件。")
+    End Sub
+End Class
