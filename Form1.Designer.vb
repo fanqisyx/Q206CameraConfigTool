e71e4b7314ebda1b30725435ec915c48dbcefc59
fanqi <fanqi@DESKTOP-EO60T56>
2023-06-25 10:30:43 +0800

    代码分块

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,13 +24,21 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.TabPage3 = New System.Windows.Forms.TabPage()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.bt_readcamera = New System.Windows.Forms.Button()
+        Me.bt_writeCamerainfoToCSV = New System.Windows.Forms.Button()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.bt_writeConfigFileinfoToCSV = New System.Windows.Forms.Button()
+        Me.bt_Readconfig = New System.Windows.Forms.Button()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
         Me.TabControl1.SuspendLayout()
         Me.TabPage1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'TabControl1
@@ -47,9 +55,8 @@ Partial Class Form1
         '
         'TabPage1
         '
-        Me.TabPage1.Controls.Add(Me.Button2)
         Me.TabPage1.Controls.Add(Me.TextBox1)
-        Me.TabPage1.Controls.Add(Me.Button1)
+        Me.TabPage1.Controls.Add(Me.Panel2)
         Me.TabPage1.Location = New System.Drawing.Point(4, 22)
         Me.TabPage1.Name = "TabPage1"
         Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
@@ -58,8 +65,47 @@ Partial Class Form1
         Me.TabPage1.Text = "相机信息读写"
         Me.TabPage1.UseVisualStyleBackColor = True
         '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Location = New System.Drawing.Point(3, 33)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(1044, 563)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.bt_readcamera)
+        Me.Panel2.Controls.Add(Me.bt_writeCamerainfoToCSV)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel2.Location = New System.Drawing.Point(3, 3)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(1044, 30)
+        Me.Panel2.TabIndex = 3
+        '
+        'bt_readcamera
+        '
+        Me.bt_readcamera.Location = New System.Drawing.Point(3, 3)
+        Me.bt_readcamera.Name = "bt_readcamera"
+        Me.bt_readcamera.Size = New System.Drawing.Size(91, 23)
+        Me.bt_readcamera.TabIndex = 0
+        Me.bt_readcamera.Text = "相机信息读取"
+        Me.bt_readcamera.UseVisualStyleBackColor = True
+        '
+        'bt_writeCamerainfoToCSV
+        '
+        Me.bt_writeCamerainfoToCSV.Location = New System.Drawing.Point(100, 3)
+        Me.bt_writeCamerainfoToCSV.Name = "bt_writeCamerainfoToCSV"
+        Me.bt_writeCamerainfoToCSV.Size = New System.Drawing.Size(91, 23)
+        Me.bt_writeCamerainfoToCSV.TabIndex = 2
+        Me.bt_writeCamerainfoToCSV.Text = "写入csv"
+        Me.bt_writeCamerainfoToCSV.UseVisualStyleBackColor = True
+        '
         'TabPage2
         '
+        Me.TabPage2.Controls.Add(Me.TextBox2)
+        Me.TabPage2.Controls.Add(Me.Panel1)
         Me.TabPage2.Location = New System.Drawing.Point(4, 22)
         Me.TabPage2.Name = "TabPage2"
         Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
@@ -67,6 +113,43 @@ Partial Class Form1
         Me.TabPage2.TabIndex = 1
         Me.TabPage2.Text = "本地信息读写"
         Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox2.Location = New System.Drawing.Point(3, 35)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(1044, 561)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.bt_writeConfigFileinfoToCSV)
+        Me.Panel1.Controls.Add(Me.bt_Readconfig)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(3, 3)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1044, 32)
+        Me.Panel1.TabIndex = 3
+        '
+        'bt_writeConfigFileinfoToCSV
+        '
+        Me.bt_writeConfigFileinfoToCSV.Location = New System.Drawing.Point(130, 3)
+        Me.bt_writeConfigFileinfoToCSV.Name = "bt_writeConfigFileinfoToCSV"
+        Me.bt_writeConfigFileinfoToCSV.Size = New System.Drawing.Size(91, 23)
+        Me.bt_writeConfigFileinfoToCSV.TabIndex = 3
+        Me.bt_writeConfigFileinfoToCSV.Text = "写入csv"
+        Me.bt_writeConfigFileinfoToCSV.UseVisualStyleBackColor = True
+        '
+        'bt_Readconfig
+        '
+        Me.bt_Readconfig.Location = New System.Drawing.Point(5, 3)
+        Me.bt_Readconfig.Name = "bt_Readconfig"
+        Me.bt_Readconfig.Size = New System.Drawing.Size(119, 23)
+        Me.bt_Readconfig.TabIndex = 0
+        Me.bt_Readconfig.Text = "读取本地配置文件"
+        Me.bt_Readconfig.UseVisualStyleBackColor = True
         '
         'TabPage3
         '
@@ -76,33 +159,6 @@ Partial Class Form1
         Me.TabPage3.TabIndex = 2
         Me.TabPage3.Text = "汇总信息读写"
         Me.TabPage3.UseVisualStyleBackColor = True
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(8, 6)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(91, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "相机信息读取"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.TextBox1.Location = New System.Drawing.Point(3, 35)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(1044, 561)
-        Me.TextBox1.TabIndex = 1
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(105, 6)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(91, 23)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = "写入csv"
-        Me.Button2.UseVisualStyleBackColor = True
         '
         'Form1
         '
@@ -115,6 +171,10 @@ Partial Class Form1
         Me.TabControl1.ResumeLayout(False)
         Me.TabPage1.ResumeLayout(False)
         Me.TabPage1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -122,8 +182,13 @@ Partial Class Form1
     Friend WithEvents TabControl1 As TabControl
     Friend WithEvents TabPage1 As TabPage
     Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents bt_readcamera As Button
     Friend WithEvents TabPage2 As TabPage
     Friend WithEvents TabPage3 As TabPage
-    Friend WithEvents Button2 As Button
+    Friend WithEvents bt_writeCamerainfoToCSV As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents bt_Readconfig As Button
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents bt_writeConfigFileinfoToCSV As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,5 +1,8 @@
 ﻿Imports System.IO
+Imports ChuangChi
 Public Class Form1
+
+#Region "辅助功能"
     'textbox：
     Delegate Sub AddShowTextBoxDelegate(textbox As System.Windows.Forms.TextBox, str As String) '//托管程序
     ''' <summary>
@@ -16,7 +19,10 @@ Public Class Form1
             textbox.Text = str
         End If
     End Sub
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+#End Region
+
+#Region "相机信息相关"
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles bt_readcamera.Click
         GetCamerainfo()
     End Sub
 
@@ -46,29 +52,100 @@ Public Class Form1
         End Try
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles bt_writeCamerainfoToCSV.Click
         Dim filePath As String = "LocalCameraInfo.csv"
         Dim content As String = TextBox1.Text
+        SaveCSV(filePath, content)
+        '' 检查文件是否存在
+        'If File.Exists(filePath) Then
+        '    Dim result As DialogResult = MessageBox.Show("文件已存在。是否覆盖？", "提示", MessageBoxButtons.YesNo)
 
+        '    If result = DialogResult.Yes Then
+        '        WriteToFile(filePath, content, True) ' 覆盖现有文件
+        '    End If
+        'Else
+        '    WriteToFile(filePath, content, False) ' 创建新文件
+        'End If
+    End Sub
+#End Region
+
+#Region "CSV操作"
+    Private Sub SaveCSV(CSVfilename As String, mytext As String)
         ' 检查文件是否存在
-        If File.Exists(filePath) Then
+        If File.Exists(CSVfilename) Then
             Dim result As DialogResult = MessageBox.Show("文件已存在。是否覆盖？", "提示", MessageBoxButtons.YesNo)
-
             If result = DialogResult.Yes Then
-                WriteToFile(filePath, content, True) ' 覆盖现有文件
+                WriteToFile(CSVfilename, mytext, True) ' 覆盖现有文件
             End If
         Else
-            WriteToFile(filePath, content, False) ' 创建新文件
+            WriteToFile(CSVfilename, mytext, False) ' 创建新文件
         End If
     End Sub
-
-
-
     Sub WriteToFile(ByVal filePath As String, ByVal content As String, ByVal overwrite As Boolean)
         Using writer As StreamWriter = New StreamWriter(filePath, overwrite)
             writer.Write(content)
         End Using
-
         Console.WriteLine("内容已写入文件。")
     End Sub
+
+#End Region
+
+#Region "本地已有config文件操作"
+    Public IniFiles() As String
+    Private Sub bt_Readconfig_Click(sender As Object, e As EventArgs) Handles bt_Readconfig.Click
+        IniFiles = FindIniFiles()
+        Dim info As String = "index,DeviceName,Exposure" + Environment.NewLine
+        Dim n As Integer = 0
+        For Each IniFile In IniFiles
+            Dim myindex As Integer = GetIndex(IniFile)
+            info += myindex.ToString() + "," + GetIniParameter(IniFile) + Environment.NewLine
+        Next
+        DG_ChangeTextBoxValue(TextBox2, info)
+    End Sub
+    Private Function GetIndex(ConfigFilePath As String) As Integer
+        Dim fileName As String = Path.GetFileNameWithoutExtension(ConfigFilePath)
+        Dim idstring As String = fileName.Remove(0, 6)
+        Dim id As Integer = Integer.Parse(idstring)
+        Return id
+    End Function
+
+    Private Function FindIniFiles() As String()
+        Dim folderPath As String = Application.StartupPath ' 获取程序所在文件夹路径
+        Dim files() As String = Directory.GetFiles(folderPath, "*.ini") ' 获取所有INI文件
+        Dim myfiles As List(Of String) = New List(Of String)
+        For Each file As String In files
+            Dim fileName As String = Path.GetFileNameWithoutExtension(file) ' 获取文件名（不包括扩展名）
+            If fileName.StartsWith("Camera") AndAlso fileName.Substring(6).All(Function(c) Char.IsDigit(c)) Then
+                myfiles.Add(file)
+            End If
+        Next
+        Return myfiles.ToArray()
+    End Function
+
+    Public Function GetIniParameter(ConfigFilePath As String) As String
+        Dim myini As ChuangChi.CC_IniFileIO = New CC_IniFileIO()
+        myini.Path = ConfigFilePath
+        Dim myinfo As String = ""
+        myinfo += myini.Read("DevicePara", "DeviceName", CC_IniFileIO.mytype.CC_String) + ","
+        myinfo += myini.Read("DevicePara", "Exposure", CC_IniFileIO.mytype.CC_String)
+        Return myinfo
+    End Function
+
+    Private Sub bt_writeConfigFileinfoToCSV_Click(sender As Object, e As EventArgs) Handles bt_writeConfigFileinfoToCSV.Click
+        Dim filePath As String = "LocalConfigFileInfo.csv"
+        Dim content As String = TextBox2.Text
+        SaveCSV(filePath, content)
+        '' 检查文件是否存在
+        'If File.Exists(filePath) Then
+        '    Dim result As DialogResult = MessageBox.Show("文件已存在。是否覆盖？", "提示", MessageBoxButtons.YesNo)
+
+        '    If result = DialogResult.Yes Then
+        '        WriteToFile(filePath, content, True) ' 覆盖现有文件
+        '    End If
+        'Else
+        '    WriteToFile(filePath, content, False) ' 创建新文件
+        'End If
+    End Sub
+#End Region
+
 End Class
